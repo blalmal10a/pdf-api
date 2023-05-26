421abf169f7f54a04c63a1bd752ef495cb351557
B. Lalmalsawma <sirukla.in@gmail.com>
2023-05-27 03:03:24 +0530

    update env, args

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const path = require('path');
-const puppeteer = require('puppeteer');
-const express = require('express');
+const path = require("path");
+const puppeteer = require("puppeteer");
+const express = require("express");
 const app = express();
-
-const router = express.Router()
+require('dotenv').config();
+const router = express.Router();
 // app.get('/', async (req, res) => {
 //     res.sendFile('./index.html')
 //     // res.send({
@@ -11,58 +11,57 @@ const router = express.Router()
 //     // })
 // })
 
-
-router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/index.html"));
     //__dirname : It will resolve to your project folder.
 });
-app.use('/', router);
+app.use("/", router);
 app.get(`/:filename`, async (req, res) => {
-
-
     try {
-
         const browser = await puppeteer.launch({
-            headless: 'new'
+            args: [
+                '--disable-setuid-sandbox',
+                '--no-sandbox',
+                '--single-process',
+                'no-zygote',
+            ],
+            executablePath:
+                process.env?.NODE_ENV === "production"
+                    ? process.env.PUPPETEER_EXECUTABLE_PATH
+                    : puppeteer.executablePath(),
+            headless: "new"
         });
         const page = await browser.newPage();
 
-
         await page.goto(`${req.query?.url}`, {
-            waitUntil: 'networkidle2'
-        })
+            waitUntil: "networkidle2"
+        });
 
         const pdf = await page.pdf({
             // path: `${req.query?.name ?? `siruk_lain_${Date.now()}`}.pdf`,
-            path: 'output.pdf',
-            format: 'A4',
+            path: "output.pdf",
+            format: "A4",
             printBackground: true,
-            encoding: 'base64'
-        })
+            encoding: "base64"
+        });
         // const pdfStream = Buffer.from(pdf, 'binary');
         // res.send(pdfStream)
         // if (req.download);
         res.set({
             // 'Content-Disposition': 'attachment; filename="output.pdf"',
-            'Content-Type': 'application/pdf',
-            'Content-Length': pdf.length
+            "Content-Type": "application/pdf",
+            "Content-Length": pdf.length
         });
         res.send(pdf);
 
-
-
-
         await browser.close();
-
     } catch (error) {
-        console.log(error.message)
+        console.log(error.message);
         res.send({
             status: 422,
             message: `Invalid URL, make sure you include 'http/https' in your link \n error message is: ${error.message}`
         });
-
     }
-
 });
 app.listen(3000, () => {
     console.log("Server started");
