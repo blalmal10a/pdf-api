207b4c47e735bc5257778ecb2b58496420512714
B. Lalmalsawma <sirukla.in@gmail.com>
2023-05-27 03:45:42 +0530

    update pdf generate function

--- index.js
+++ index.js
@@ -13,61 +13,10 @@ const router = express.Router();
 
 router.get("/", function (req, res) {
     res.sendFile(path.join(__dirname + "/index.html"));
-    //__dirname : It will resolve to your project folder.
 });
 app.use("/", router);
-// app.get(`/:filename`, async (req, res) => {
-//     try {
-//         const browser = await puppeteer.launch({
-//             args: [
-//                 '--disable-setuid-sandbox',
-//                 '--no-sandbox',
-//                 '--single-process',
-//                 'no-zygote',
-//             ],
-//             executablePath:
-//                 process.env?.NODE_ENV === "production"
-//                     ? process.env.PUPPETEER_EXECUTABLE_PATH
-//                     : puppeteer.executablePath(),
-//             headless: "new"
-//         });
-//         const page = await browser.newPage();
-
-//         await page.goto(`${req.query?.url}`, {
-//             waitUntil: "networkidle2"
-//         });
-
-//         const pdf = await page.pdf({
-//             // path: `${req.query?.name ?? `siruk_lain_${Date.now()}`}.pdf`,
-//             path: "output.pdf",
-//             format: "A4",
-//             printBackground: true,
-//             encoding: "base64"
-//         });
-//         // const pdfStream = Buffer.from(pdf, 'binary');
-//         // res.send(pdfStream)
-//         // if (req.download);
-//         res.set({
-//             // 'Content-Disposition': 'attachment; filename="output.pdf"',
-//             "Content-Type": "application/pdf",
-//             "Content-Length": pdf.length
-//         });
-//         res.send(pdf);
-
-//         await browser.close();
-//     } catch (error) {
-//         console.log(error.message);
-//         res.send({
-//             status: 422,
-//             message: `Invalid URL, make sure you include 'http/https' in your link \n error message is: ${error.message}`
-//         });
-//     }
-// });
-
-
-app.get(`/test`, async (req, res) => {
+app.get(`/generate_pdf`, async (req, res) => {
     try {
-
         const browser = await puppeteer.launch({
             args: [
                 '--disable-setuid-sandbox',
@@ -75,15 +24,15 @@ app.get(`/test`, async (req, res) => {
                 '--single-process',
                 'no-zygote',
             ],
-            executablePath: '/usr/bin/google-chrome-stable',
-            // process.env?.NODE_ENV === "production"
-            //     ? '/usr/bin/google-chrome-stable'
-            //     : puppeteer.executablePath(),
+            executablePath:
+                process.env?.NODE_ENV === "production"
+                    ? process.env.PUPPETEER_EXECUTABLE_PATH
+                    : puppeteer.executablePath(),
             headless: "new"
         });
         const page = await browser.newPage();
 
-        await page.goto(`https://sirukla.in`, {
+        await page.goto(`${req.query?.url}`, {
             waitUntil: "networkidle2"
         });
 
@@ -113,6 +62,103 @@ app.get(`/test`, async (req, res) => {
         });
     }
 });
+
+app.get(`/generate_pdf/:filename`, async (req, res) => {
+    try {
+        const browser = await puppeteer.launch({
+            args: [
+                '--disable-setuid-sandbox',
+                '--no-sandbox',
+                '--single-process',
+                'no-zygote',
+            ],
+            executablePath:
+                process.env?.NODE_ENV === "production"
+                    ? process.env.PUPPETEER_EXECUTABLE_PATH
+                    : puppeteer.executablePath(),
+            headless: "new"
+        });
+        const page = await browser.newPage();
+
+        await page.goto(`${req.query?.url}`, {
+            waitUntil: "networkidle2"
+        });
+
+        const pdf = await page.pdf({
+            // path: `${req.query?.name ?? `siruk_lain_${Date.now()}`}.pdf`,
+            path: "output.pdf",
+            format: "A4",
+            printBackground: true,
+            encoding: "base64"
+        });
+        // const pdfStream = Buffer.from(pdf, 'binary');
+        // res.send(pdfStream)
+        // if (req.download);
+        res.set({
+            // 'Content-Disposition': 'attachment; filename="output.pdf"',
+            "Content-Type": "application/pdf",
+            "Content-Length": pdf.length
+        });
+        res.send(pdf);
+
+        await browser.close();
+    } catch (error) {
+        console.log(error.message);
+        res.send({
+            status: 422,
+            message: `Invalid URL, make sure you include 'http/https' in your link \n error message is: ${error.message}`
+        });
+    }
+});
+
+// app.get(`/test`, async (req, res) => {
+//     try {
+
+//         const browser = await puppeteer.launch({
+//             args: [
+//                 '--disable-setuid-sandbox',
+//                 '--no-sandbox',
+//                 '--single-process',
+//                 'no-zygote',
+//             ],
+//             executablePath: '/usr/bin/google-chrome-stable',
+//             // process.env?.NODE_ENV === "production"
+//             //     ? '/usr/bin/google-chrome-stable'
+//             //     : puppeteer.executablePath(),
+//             headless: "new"
+//         });
+//         const page = await browser.newPage();
+
+//         await page.goto(`https://sirukla.in`, {
+//             waitUntil: "networkidle2"
+//         });
+
+//         const pdf = await page.pdf({
+//             // path: `${req.query?.name ?? `siruk_lain_${Date.now()}`}.pdf`,
+//             path: "output.pdf",
+//             format: "A4",
+//             printBackground: true,
+//             encoding: "base64"
+//         });
+//         // const pdfStream = Buffer.from(pdf, 'binary');
+//         // res.send(pdfStream)
+//         // if (req.download);
+//         res.set({
+//             // 'Content-Disposition': 'attachment; filename="output.pdf"',
+//             "Content-Type": "application/pdf",
+//             "Content-Length": pdf.length
+//         });
+//         res.send(pdf);
+
+//         await browser.close();
+//     } catch (error) {
+//         console.log(error.message);
+//         res.send({
+//             status: 422,
+//             message: `Invalid URL, make sure you include 'http/https' in your link \n error message is: ${error.message}`
+//         });
+//     }
+// });
 app.listen(3000, () => {
     console.log("Server started");
 });
